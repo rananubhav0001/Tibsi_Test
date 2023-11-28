a4b058766b0d477fa1f8867c8effd5ab309b0a7b
Anubhav Rana <anubhav.r@idsil.com>
2023-11-28 15:26:39 +0530

    changes in ProtectedPage

--- src/components/ProtectedPage.js
+++ src/components/ProtectedPage.js
@@ -1,165 +1,19 @@
-// import { Layout, Menu, theme } from "antd";
-// import Sider from "antd/es/layout/Sider";
-// import { Content } from "antd/es/layout/layout";
-// import React from "react";
-// import { PieChartOutlined, ProjectOutlined } from "@ant-design/icons";
-
-// const ProtectedPage = ({ children }) => {
-
-//   const {
-//     token: { colorBgContainer },
-//   } = theme.useToken();
-
-//   //SIDEBAR ITEMS
-//   const sidebarItems = [
-//     {
-//       key: "dashboard",
-//       label: "Dashboard",
-//       icon: <PieChartOutlined />,
-//       // onClick: () => navigate("/dashboard"),
-//     },
-//     {
-//       key: "manageUsers",
-//       label: "Manage Users",
-//       icon: <ProjectOutlined />,
-//       // onClick: () => navigate("/projectTab"),
-//       children: [
-//         {
-//           key: "addNewUser",
-//           label: "Add New User",
-//           // onClick: () => navigate("/allprojects"),
-//         },
-
-//       ].filter(Boolean),
-//     },
-//     {
-//       key: "audioFile",
-//       label: "Audio File",
-//       icon: <ProjectOutlined />,
-//       // onClick: () => navigate("/projectTab"),
-//       children: [
-//         {
-//           key: "manageAudioFileList",
-//           label: "Manage Audio File List",
-//           // onClick: () => navigate("/allprojects"),
-//         },
-//         {
-//           key: "uploadNewAudioFile",
-//           label: "Upload New Audio File",
-//           // onClick: () => navigate("/allprojects"),
-//         },
-
-//       ].filter(Boolean),
-//     },
-//     {
-//       key: "viewParameter",
-//       label: "View Parameter",
-//       icon: <PieChartOutlined />,
-//       // onClick: () => navigate("/dashboard"),
-//     },
-//     {
-//       key: "manageVisualization",
-//       label: "Manage Visualization",
-//       icon: <PieChartOutlined />,
-//       // onClick: () => navigate("/dashboard"),
-//     },
-//     {
-//       key: "viewNotification",
-//       label: "View My Notification",
-//       icon: <PieChartOutlined />,
-//       // onClick: () => navigate("/dashboard"),
-//     },
-//     {
-//       key: "reports",
-//       label: "Reports",
-//       icon: <PieChartOutlined />,
-//       // onClick: () => navigate("/dashboard"),
-//     },
-//   ];
-
-//   return (
-//     <div>
-//       <div>
-//         <Layout className="mysidebar">
-//           <Sider
-//             // style={{ background: colorBgContainer }}
-//             collapsible
-//             // collapsed={collapsed}
-//             // onCollapse={(value) => setCollapsed(value)}
-//           >
-//             <div className="demo-logo-vertical bg-white" />
-//             <Menu
-//               //   style={{ background: colorBgContainer }}
-//               theme="light"
-//               mode="inline"
-//               defaultSelectedKeys={["2"]}
-//             >
-//               {sidebarItems.map((item) =>
-//                 item.children ? (
-//                   <Menu.SubMenu
-//                     key={item.key}
-//                     title={
-//                       <span>
-//                         {item.icon}
-//                         <span>{item.label}</span>
-//                       </span>
-//                     }
-//                   >
-//                     {item.children.map((subItem) => (
-//                       <Menu.Item key={subItem.key} onClick={subItem.onClick}   style={{ lineHeight: "48px" }}>
-//                         {subItem.label}
-//                       </Menu.Item>
-//                     ))}
-//                   </Menu.SubMenu>
-//                 ) : (
-//                   <Menu.Item key={item.key} onClick={item.onClick}>
-//                     {item.icon}
-//                     <span>{item.label}</span>
-//                   </Menu.Item>
-//                 )
-//               )}
-//             </Menu>
-//           </Sider>
-//           <Layout>
-//             <Content style={{ margin: "0 1%", marginTop: "1%" }}>
-//               <div
-//                 className="mycontent  border-rounded"
-//                 style={{
-//                   height: "87vh",
-//                   //   background: colorBgContainer,
-//                   borderRadius: "5px",
-//                 }}
-//               >
-//                 {children}
-//                 {/* <Test /> */}
-//               </div>
-//             </Content>
-//           </Layout>
-//         </Layout>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProtectedPage;
-
-// ....................................
 
 import React, { useState } from "react";
 import {
   AudioOutlined,
   AuditOutlined,
-  DashboardOutlined,
-  FileOutlined,
+  // DashboardOutlined,
+  // FileOutlined,
   FolderViewOutlined,
   HighlightOutlined,
-  NotificationOutlined,
+  // NotificationOutlined,
   UploadOutlined,
-  UserAddOutlined,
-  UserOutlined,
+  // UserAddOutlined,
+  // UserOutlined,
 } from "@ant-design/icons";
 import {  Layout, Menu,  theme } from "antd";
-import HomePage from "../pages/Homepage/Homepage";
+// import HomePage from "../pages/Homepage/Homepage";
 import UploadAudio from "../pages/UploadAudio/UploadAudio";
 import { CustomDateFormat } from "./CustomDate";
 import ViewAudio from "../pages/ViewAudio/ViewAudio";
@@ -179,23 +33,23 @@ function getItem(label, key, icon, children) {
 
 //ITEMS
 const items = [
-  getItem("Dashboard", "dashboard", <DashboardOutlined />),
-  getItem("Manage User", "manageUsers", <UserOutlined />, [
-    getItem("Add New User", "addNewUser", <UserAddOutlined />),
-  ]),
+  // getItem("Dashboard", "dashboard", <DashboardOutlined />),
+  // getItem("Manage User", "manageUsers", <UserOutlined />, [
+  //   getItem("Add New User", "addNewUser", <UserAddOutlined />),
+  // ]),
+  getItem("Manage Visualisation", "manageVisualisation", <HighlightOutlined />),
+  getItem("View Parameters", "viewParameters", <FolderViewOutlined />),
   getItem("Manage Audio", "manageAudioFile", <AuditOutlined />, [
     getItem("View Audio ", "viewAudioList", <AudioOutlined />),
     getItem("Upload Audio ", "uploadNewAudio", <UploadOutlined />),
   ]),
-  getItem("View Parameters", "viewParameters", <FolderViewOutlined />),
-  getItem("Manage Visualisation", "manageVisualisation", <HighlightOutlined />),
-  getItem("View Notifications", "viewNotification", <NotificationOutlined />),
-  getItem("Reports", "reports", <FileOutlined />),
+  // getItem("View Notifications", "viewNotification", <NotificationOutlined />),
+  // getItem("Reports", "reports", <FileOutlined />),
 ];
 
 const ProtectedPage = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const [selectedMenuItem, setSelectedMenuItem] = useState("dashboard");
+  const [selectedMenuItem, setSelectedMenuItem] = useState("manageVisualisation");
 
   //HANDLE MENU CLICK
   const handleMenuClick = ({ key }) => {
@@ -205,17 +59,18 @@ const ProtectedPage = () => {
   //GET CONTENT COMPONENT
   const getContentComponent = () => {
     switch (selectedMenuItem) {
-      case "dashboard":
-        return (
-          <div>
-            <HomePage />
-          </div>
-        );
-      case "manageUsers":
-        return <div>Content for Option 2</div>;
-      case "addNewUser":
-        return <div>Content for addNewUser</div>;
-
+      // case "dashboard":
+      //   return (
+      //     <div>
+      //       <HomePage />
+      //     </div>
+      //   );
+      // case "manageUsers":
+      //   return <div>Content for Option 2</div>;
+      // case "addNewUser":
+      //   return <div>Content for addNewUser</div>;
+      case "manageVisualisation":
+        return <div><Test/></div>;
       case "viewAudioList":
         return (
           <div>
@@ -234,12 +89,11 @@ const ProtectedPage = () => {
             <ViewParameter />
           </div>
         );
-      case "manageVisualisation":
-        return <div><Test/></div>;
-      case "viewNotification":
-        return <div>Content for viewNotification</div>;
-      case "reports":
-        return <div>Content for reports</div>;
+      
+      // case "viewNotification":
+      //   return <div>Content for viewNotification</div>;
+      // case "reports":
+      //   return <div>Content for reports</div>;
       default:
         return <div>Select a menu item</div>;
     }
@@ -260,7 +114,7 @@ const ProtectedPage = () => {
           <div className="demo-logo-vertical" />
           <Menu
             theme="dark"
-            defaultSelectedKeys={["dashboard"]}
+            defaultSelectedKeys={["manageVisualisation"]}
             mode="inline"
             items={items}
             onClick={handleMenuClick}
@@ -274,7 +128,7 @@ const ProtectedPage = () => {
               </div> */}
               {/* <div className="flex justify-between items-center"> */}
               <div className="text-black">
-                <h1 style={{ color: "rgb(0,33,64)" }}>TIBSI</h1>
+                <h1 style={{ color: "rgb(0,33,64)" }}>DEMO</h1>
               </div>
               <div className="flex  items-center text-black    rounded">
                 {/* <Badge
